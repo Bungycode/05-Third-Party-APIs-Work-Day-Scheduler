9e8d369ae4209c6e8b8e45a0215e2101567c6a33
Andrew Bungcayao <thedevandrew@gmail.com>
2022-04-05 01:13:21 -0400

    added dynamic javascript and linked css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,48 @@
 headerEl = document.querySelector("#header");
+tBodyEl = document.querySelector("tbody");
 
-
-
-
-
-
-
-
-
+time = moment();
+console.log(time);
 var calendarAppTime = function () {
-    time = moment().format("MMMM Do YYYY");
-    console.log(time);
+  console.log(time);
 
-    appTime = document.createElement("p");
-    appTime.setAttribute("class", "lead");
-    appTime.textContent = time;
+  appTime = document.createElement("p");
+  appTime.setAttribute("class", "lead");
+  appTime.textContent = time.format("MMMM Do YYYY");
 
-    headerEl.appendChild(appTime);
-}
+  headerEl.appendChild(appTime);
+};
 
 calendarAppTime();
 
+function createCalendar() {
+  var currentTime = time.local().hour();
+  console.log(currentTime);
+  for (var i = 0; i < 24; i++) {
+    var calendarHour = moment(i, "H").format("hA");
+
+    tRowEl = document.createElement("tr");
+    tRowEl.innerHTML = `
+                <th class="col-1">
+                    <h4>${calendarHour}</h4>
+                </th>
+                <td class="col-10">
+                    <textarea class="col"></textarea>
+                </td>
+                <td class="col-1 saveBtn">
+                    <i class="fa fa-lock" aria-hidden="true"></i>
+                </td>
+                `;
+    tBodyEl.appendChild(tRowEl);
+    console.log(currentTime + " " + i);
+    if (currentTime === i) {
+        tRowEl.children[1].children[0].classList.add("present");
+    } else if (currentTime > i) {
+        tRowEl.children[1].children[0].classList.add("past");
+    } else {
+      tRowEl.children[1].children[0].classList.add("future");
+    }
+  }
+}
+
+createCalendar();
